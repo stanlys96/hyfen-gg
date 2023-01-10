8b44f386c9234c0a3e1d1b72d7d9df32ccd6ac13
stanlys96 <stanlyskwok@gmail.com>
2023-01-10 08:57:26 +0700

    walao eh

--- components/Home/OurProducts.jsx
+++ components/Home/OurProducts.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import useTranslation from 'next-translate/useTranslation'
 import Fade from 'react-reveal/Fade';
+import ActiveLink from '../Common/ActiveLink';
 
 export default function OurProducts() {
     const { t } = useTranslation('home')
@@ -8,12 +9,14 @@ export default function OurProducts() {
 	const items = [
 		{
 			title: t('home:hyfen_play'),
-			description1: t('home:hyfen_play_desc')
+			description1: t('home:hyfen_play_desc'),
+            link: '/hyfen-play'
 		},
 		{
 			title: t('home:hyfen_tools'),
 			description1: t('home:hyfen_tools_desc1'),
-            description2: t('home:hyfen_tools_desc2')
+            description2: t('home:hyfen_tools_desc2'),
+            link: '/hyfen-tools'
 		}
 	]
 
@@ -47,27 +50,29 @@ export default function OurProducts() {
                         top
                         delay={i !== 0 ? i * 200 : 0}
                     >
-                        <div className="group relative products-container overflow-hidden">
-                            <div className={`bg-product-${i + 1} group-hover:w-5/6 transition-all duration-500 py-8 md:py-14 px-2 md:px-12 relative`}>
-                                <div className="container flex justify-around items-center mx-auto relative z-50 gap-12">
-                                    <div className="relative grid grid-rows-2">
-                                        <p className="text-xl lg:text-4xl font-bold mb-4">{item.title}</p>
-                                        <p className="text-base lg:text-xl text-ellipsis">{item.description1} {item.description2 && item.description2}</p>
+                        <ActiveLink href={item.link}>
+                            <div className="group relative products-container overflow-hidden cursor-pointer">
+                                <div className={`bg-product-${i + 1} group-hover:w-5/6 transition-all duration-500 py-8 md:py-14 px-2 md:px-12 relative`}>
+                                    <div className="container flex justify-around items-center mx-auto relative z-50 gap-12">
+                                        <div className="relative grid grid-rows-2">
+                                            <p className="text-xl lg:text-4xl font-bold mb-4">{item.title}</p>
+                                            <p className="text-base lg:text-xl text-ellipsis">{item.description1} {item.description2 && item.description2}</p>
+                                        </div>
+                                        <img
+                                            src={`/images/home/arrow-right.svg`}
+                                            className="h-fit"
+                                            width="15%"
+                                        />
                                     </div>
-                                    <img
-                                        src={`/images/home/arrow-right.svg`}
-                                        className="h-fit"
-                                        width="15%"
-                                    />
                                 </div>
+                                <div className="absolute top-0 -z-10 z-minus-5 product-img-container right-0">
+                                        <img
+                                            src={`/images/home/product-${(i + 1) + (windowWidth > 1024 ? "" : '-mobile-screen')}.png`}
+                                            className="h-full w-full z-minus-5"
+                                        />
+                                    </div>
                             </div>
-                            <div className="absolute top-0 -z-10 z-minus-5 product-img-container right-0">
-                                    <img
-                                        src={`/images/home/product-${(i + 1) + (windowWidth > 1024 ? "" : '-mobile-screen')}.png`}
-                                        className="h-full w-full z-minus-5"
-                                    />
-                                </div>
-                        </div>
+                        </ActiveLink>
                     </Fade>
                 ))
             }

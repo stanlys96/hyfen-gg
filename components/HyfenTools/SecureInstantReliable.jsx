2eb377efc3bfbda274b2a394f2986858f2b464ba
muchtarama <muchtar@hyfen.gg>
2023-01-17 09:45:57 +0700

    add padding in hyfen tools page

--- components/HyfenTools/SecureInstantReliable.jsx
+++ components/HyfenTools/SecureInstantReliable.jsx
@@ -9,7 +9,7 @@ export default function SecureInstantReliable() {
 	const { t } = useTranslation('hyfen-tools')
 	return (
 		<Section size='lg'>
-			<div className='container mx-auto max-w-7xl px-0'>
+			<div className='container mx-auto max-w-7xl relative'>
 				<div className='grid px-0 grid-cols-1 lg:grid-cols-3 gap-5 xl:gap-10 gap-y-16 my-12 md:mt-0'>
 					{secureInstantReliable.map((item, index) => (
 						<Fade key={item.code} bottom delay={index === 0 ? 0 : index * 100}>

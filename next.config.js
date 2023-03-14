54e1aa88e19ae1997ba11117d712829a4b7e03ae
Wawan PBS01 <stanlyskwok@gmail.com>
2023-03-14 11:10:28 +0700

    add robots.txt to api

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextTranslate = require('next-translate')
 
 module.exports = nextTranslate({
 	reactStrictMode: true,
+	async rewrites() {
+		return [
+			{
+				source: '/robots.txt',
+				destination: '/api/robots',
+			},
+		]
+	},
 	env: {
 		BASE_URL: process.env.BASE_URL,
 	},
